f3c01980ebcd4504169bbc23254ade633f3577f8
durgesh02 <jarvisjs283@gmail.com>
2025-02-08 14:43:32 +0530

    upddated

--- eCoading/asgi.py
+++ eCoading/asgi.py
@@ -1,10 +1,12 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from Room.routing import websocket_urlpatterns  # Import WebSocket URLs
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eCoading.settings")
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
